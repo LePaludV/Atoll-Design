8bfbafa5782665aee4af5321df086c37ec75561a
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-07-27 14:11:09 +0200

    Initial commit

---
+++ JavaScript/DragAndDrop.js
@@ -0,0 +1,75 @@
+    
+$(function() {    
+    document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
+    });
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
+    });
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
+    
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+  
+
+    
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+        
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
+    }
+  
+   
+  
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log(`url('${reader.result}')`)
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+        $('.main').css('background-image',thumbnailElement.style.backgroundImage);
+      };
+    } 
+      thumbnailElement.style.display='none';
+      $('input').disabled
+  }
+});
